b853599ab2977e87bca4f070945c75c48ba5dfec
ABHAY KRISHNA G <abhayganesh154@gmail.com>
2025-03-02 23:29:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
 
-function toggleCertifications() {
-    var moreCerts = document.getElementById("moreCerts");
-    var btn = document.getElementById("seeMoreBtn");
-    if (moreCerts.style.display === "none") {
-      moreCerts.style.display = "block";
-      btn.innerText = "See Less";
-    } else {
-      moreCerts.style.display = "none";
-      btn.innerText = "See More";
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  var moreCerts = document.getElementById("moreCerts");
+  var btn = document.getElementById("seeMoreBtn");
+
+  if (btn) {
+    btn.addEventListener("click", function () {
+      if (moreCerts.style.display === "none" || moreCerts.style.display === "") {
+        moreCerts.style.display = "block";
+        btn.innerText = "See Less";
+      } else {
+        moreCerts.style.display = "none";
+        btn.innerText = "See More";
+      }
+    });
   }
+});
